f458e1c8065031c8f1e7175ccae779142d2d0ed2
boydco <boydcoli@gmail.com>
2019-10-27 11:46:14 -0500

    added directory and menu-item components and replaced those in app.js

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+
+import MenuItem from '../menu-item/menu-item.component';
+
+import './directory.styles.scss';
+
+class Directory extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            sections: [{
+                title : 'hats',
+                imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+                id: 1
+            },
+            {
+                title : 'jackets',
+                imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+                id: 2
+            },
+            {
+                title : 'shows',
+                imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+                id: 3
+            },
+            {
+                title : 'women',
+                imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+                size: 'large',
+                id: 4
+            },
+            {
+                title : 'mens',
+                imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+                size: 'large',
+                id: 5
+            }
+        ]
+        }
+    }
+
+    render() {
+        return (
+            <div className='directory-menu'>
+                {
+                    this.state.sections.map(({ title, imageUrl, id, size }) =>(
+                        <MenuItem key={id} title={title} imageUrl={imageUrl} size={size}/>
+                    ))
+                }
+            </div>
+        );
+    }
+}
+
+
+export default Directory;
